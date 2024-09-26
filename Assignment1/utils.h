d86bf17380caf10ad83ec7da56b888625257e052
Aj Donald <anthony.donald0427@gmail.com>
2024-09-26 08:30:15 -0400

    added needed utils for logging, etc

---
+++ Assignment1/utils.h
@@ -0,0 +1,27 @@
+/**
+ * Utilities used for Assignemnt 1
+ * 
+ * Declaring functions, structures
+ * 
+ */
+#include <iostream>  
+#include <stdint.h> 
+#
+
+#ifndef UTILS_H
+#define UTILS_H
+
+
+struct EventType
+{
+    std::string event; 
+    uint8_t eventID; 
+    uint32_t duration; 
+}; 
+
+struct VectorTableEntry { 
+    uint32_t interruptNum; 
+    uint32_t ISRAddress; 
+};
+
+#endif
