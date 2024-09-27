ce36575690df406da018f19014f067285cec0b9f
Aj Donald <anthony.donald0427@gmail.com>
2024-09-27 10:18:00 -0400

    made changes to vectorEntry structure (eliminated) since we can get the interrupt num from event.ID

--- Assignment1/functions.cpp
+++ Assignment1/functions.cpp
@@ -16,31 +16,49 @@
 // Global variable to track simulation time
 static uint32_t sim_time = 0;
 
+
 void eventHandler(TraceEvent event)
 {
+    std::vector<uint16_t> isrAddresses = vectorTableHandler("vector_table.txt");
     if (event.name == "CPU") {
         event.name = "CPU Execution";
         logExecution(event.duration, event.name);
     }
-    else if (event.name == "SYSCALL") {
-        // Mode switching = 1ms
-        logExecution(1, "Switch to Kernel Mode");
+    if(event.ID < isrAddresses.size())
+    {
+        uint16_t ISRAddress = isrAddresses[event.ID];
+        uint16_t memoryPosition = event.ID * 2; 
+        
+        if (event.name == "SYSCALL") {
+            // Mode switching = 1ms
+            logExecution(1, "Switch to Kernel Mode");
 
-        // Save context is random between 1 - 3 ms
-        logExecution(rand() % 3 + 1, "Save Context");
-        logExecution(1, "Find vector in memory");
+            // Save context is random between 1 - 3 ms
+            logExecution(rand() % 3 + 1, "Save Context");
+            logExecution(1, "Find vector #"+std::to_string(event.ID) + " in memory position 0x"+std::to_string(memoryPosition));
+            logExecution(1, "Load address 0x"+std::to_string(ISRAddress)+ " into PC")
 
-        // Execute ISR and IRET
-        logExecution(event.duration, "Execute ISR");
-        logExecution(1, "IRET");
-    }
-    else if (event.name == "END_IO") {
-        logExecution(event.duration, "I/O Completed");
+            // Execute ISR and IRET
+            logExecution(event.duration, "SYSCALL: run the ISR");
+            logExecution(1, "IRET");
+        }
+        else if (event.name == "END_IO") {
+            logExecution(1, "Check the priority of the Interrupt");
+            logExecution(1, "Check if the interrupt is masked"); 
+            logExecution(1, "Switch to Kernel Mode"); 
+            logExecution(rand() % 3 + 1, "Save Context"); 
+            logExecution(1, "Find vector #"+std::to_string(event.ID)+ " in memory position 0x"+std::to_string(memoryPosition))
+                logExecution(1, "Load address 0x" + std::to_string(ISRAddress));
+            logExecution(event.duration, "I/O Completed");
+            logExecution(1, "IRET");
+        }
     }
 }
 
+
 // Function to log the execution of each event
 void logExecution(uint32_t duration, const std::string eventName) {
+    
     // Open the file in append mode
     std::ofstream outputFile("execution.txt", std::ios::app);
 
@@ -107,9 +125,33 @@ void inputRead(std::string fileName){
     
     inputFile.close(); 
 
-    // Output the parsed events for verification
+    // Output the events for verification
     for (const auto& event : events)
     {
         eventHandler(event);
     }
+}
+
+std::vector<uint16_t> vectorTableHandler(std::string fileName)
+{
+    std::vector<uint16_t> isrAddresses; 
+
+    std::ifstream inputFile(fileName);
+
+    std::string line; 
+
+    while(getfile(inputFile, line))
+    {
+        std::istringstream iss(line); 
+
+        uint16_t ISRAddress; 
+
+        iss >> std::hex >> ISRAddress; 
+
+        isrAddresses.push_back(ISRAddress);
+    }
+    inputFile.close();
+
+    return isrAddresses;
+    
 }
