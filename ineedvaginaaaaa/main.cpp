9393ae62d37390b949e357d38f3420df0937d7ac
ajdonald <ajdonald@LABS.SCE.CARLETON.CA>
2024-09-30 10:12:37 -0400

    j'aime beaucoup fornite les amis

---
+++ ineedvaginaaaaa/main.cpp
@@ -0,0 +1,19 @@
+
+#include "functions.h"
+int main()
+{
+    srand(time(0));
+
+    std::string vectorFileName, TraceFile; 
+
+    std::cout << "Enter the vector table file name: "; 
+    std::cin >> vectorFileName; 
+    
+
+    std::cout << "Enter the trace file name: ";
+    std::cin >> TraceFile; 
+
+    inputRead(TraceFile, vectorFileName);
+    
+    return 0;
+}
