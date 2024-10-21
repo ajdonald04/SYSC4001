d024459dcee7295f64ce190e0e56a28f13ff56a4
Aj Donald <anthony.donald0427@gmail.com>
2024-10-21 12:14:32 -0400

    added support for part 4, commented code and cleanup up existing code.

--- A2/Part2/ParentProcess.cpp
+++ A2/Part2/ParentProcess.cpp
@@ -1,53 +1,73 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void){
+int main() {
 
+    // generate a key to use as an "id" for the shared memory
+    key_t key = ftok("shmfile", 65); 
     
-    pid_t pid = fork();
-
-    if (pid < 0) {
-        std::cerr << "Fork failed!" << std::endl;
+    // Create shared memory segment (store it in shmid)
+    // 0666 is a permission mode OR IPC_CREAT means create if it doenst exist.
+    int shmid = shmget(key, sizeof(int), 0666 | IPC_CREAT); // shmget, allocate the shared memory, 
+    if (shmid < 0) {
+        cerr << "Failed to create shared memory" << endl;
         exit(1);
     }
 
-    
-    else if(pid == 0){
+    // link the shared memory segment
+    int *shared_var = (int *)shmat(shmid, nullptr, 0);
+    if (shared_var == (void *)-1) {
+        cerr << "Failed to attach shared memory" << endl;
+        exit(1);
+    }
 
-        while(1){
-            for(int j=0; j< 1; j++){
-                //waitpid(pids[j], nullptr, 0);
-                cout << "I am process " << j + 1 << endl;
-                
+    // Initialize the shared variable
+    *shared_var = -1;
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        cerr << "Fork failed!" << endl;
+        exit(1);
+    } else if (pid == 0) {
+        while (*shared_var != 0) {
+             // generate random number between 0 and 10
+            *shared_var = rand() % 11;
+            cout << "Process 1: Generated random number: " << *shared_var << endl;
+
+            if (*shared_var > 5) {
+                cout << "Process 1: High value" << endl;
+            } else {
+                cout << "Process 1: Low value" << endl;
             }
 
-            int randomNum = rand() %11;
-            if (randomNum > 5){
-                cout << "High value" <<endl;
+            // execute process 2 if the number is 9
+            if (*shared_var == 9) {
+                execl("/Users/jayven/sysc4001/A2/Part2/process2", "process2", nullptr);
+                cerr << "Failed to launch Process 2!" << endl;
+                exit(1);
             }
 
-            else if(randomNum <=5){
-                cout<<"Low value"<<  endl;
-            }
-            if (randomNum == 9){
-                
-                execl("/Users/jayven/sysc4001/A2/Part2/process2","process2",nullptr);
-                std::cerr << "Failed to launch Process 2!" << std::endl;
-            }
-            sleep(1);
+            sleep(1); 
         }
-    }
 
-    else if (pid > 0){
-        waitpid(pid, nullptr,0);
+        // shmdt is used to detach the memory 
+        shmdt(shared_var);
         cout << "Process 1 finished" << endl;
+    } else {
+        // Parent waits for child to finish 
+        waitpid(pid, nullptr, 0);
+
+        // cleanup shared memory fter processes are finished
+        shmctl(shmid, IPC_RMID, nullptr);
+        cout << "Parent cleaned up shared memory" << endl;
     }
-
-   
-
-
 
     return 0;
 }
--- A2/Part2/process2.cpp
+++ A2/Part2/process2.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 #include <unistd.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
 
-int main(void){
-    for(int i =0; i<10; i++){
+int main() {
+    
+    // similarly to the other file, create a key before shared memory allocation occurs (shmget)
+    key_t key = ftok("shmfile", 65); // same key as P1
+    int shmid = shmget(key, sizeof(int), 0666); 
+    
+    // checking validity
+    if (shmid < 0) {
+        cerr << "Process 2: Failed to access shared memory" << endl;
+        exit(1);
+    }
 
-        cout << "I am process 2!" << endl;
+    // attach the memory segment
+    int *shared_var = (int *)shmat(shmid, nullptr, 0);
+
+    // error / validity checking, ensuring memory is attached correctly
+    if (shared_var == (void *)-1) {
+        cerr << "Process 2: Failed to attach shared memory" << endl;
+        exit(1);
+    }
+
+    // loop until the value in shared memory is zero
+    while (*shared_var != 0) {
+        cout << "Process 2: Shared random number is: " << *shared_var << endl;
         sleep(1);
     }
 
+    // detach shared memory
+    shmdt(shared_var);
+    cout << "Process 2 finished" << endl;
+
     return 0;
 }
-
