bce98fb296c8ffdd1820b9c4ddac60ddcc5427e5
Jayven <jayvenlarsen04@gmail.com>
2024-12-02 08:37:23 -0500

    Deleted unneeded files and pushed read me

--- A2/Part3/test_AjDonald_JayvenLarsen.sh
+++
@@ -1,7 +0,0 @@
-g++ -std=c++11 interrupts_AjDonald_JayvenLarsen.cpp -I interrupts_AjDonald_JayvenLarsen.hpp -o test1.o 
-./test1.o<<EOF 
-vector_table_AjDonald_JayvenLarsen.txt
-trace_AjDonald_JayvenLarsen.txt
-execution_AjDonald_JayvenLarsen
-external_files_AjDonald_JayvenLarsen.txt
-EOF 
